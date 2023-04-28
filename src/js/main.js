79f638e8a479ac4bfe1ebc14040500d4260ffad3
lucas <lucasgominhaa448@gmail.com>
2023-04-28 14:05:16 -0300

    added new things

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-const apiKey = "NsILUexllW1GPxsdbcaA2cGvRkPGE3aqKhZizmbl";
+/*const apiKey = "NsILUexllW1GPxsdbcaA2cGvRkPGE3aqKhZizmbl";
 const url =
   "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY";
 const url2 = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
@@ -9,4 +9,4 @@ function callUrl(url) {
     .then(json => console.log(json));
 }
 
-callUrl(url);
+callUrl(url);*/
